61dac01a73d3d3ae46d70488261226a77c2b798c
ginix909 <74022199+ginix909@users.noreply.github.com>
2021-01-21 12:28:45 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,28 +1,31 @@
 from collections import Counter
 
-encoded_string = list('sos')
-latin_alphabet = list('abcdefghijklmnopqrstuvwxyz')
+input_string = str(input())
 
 def is_in_latin_alphabet():
-    if 2 <= len(encoded_string) <= 1000:
-        for x in encoded_string:
+    latin_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    if 2 <= len(input_string) <= 1000:
+        for x in input_string:
             if x in latin_alphabet:
                 continue
             else:
-                ValueError
-                print(f'Symbol {x} is not in Latin Alphabet')
+                raise ValueError(f'Symbol {x} is not in Latin Alphabet')
     else:
-        print(f'Количество символов в строке {len(encoded_string)}, ожидается от 2 до 1000')
+        print(f'Количество символов в строке {len(input_string)}, ожидается от 2 до 1000')
 
 def searching_letter():
+    is_in_latin_alphabet()
     result_list =[]
     result_string = ''
-    counter_occurrences = dict(Counter(encoded_string))
-
+    counter_occurrences = dict(Counter(input_string))
     for k, v in counter_occurrences.items():
         if v >= 2:
             result_list.append(k)
-    result_string = result_string.join(result_list)
-    print(result_string)
+            result_string = ''.join(result_list)
+
+    if result_string == '':
+        print('В введенной строке нет объектов, встречающихся 2 или более раз')
+    else:
+        print(result_string)
 
 searching_letter()
